ce88d1ba91900cccdc3e4cd43ed991eb0c1df064
Jerome_D <38654124+jeromedejaegher@users.noreply.github.com>
2024-07-13 03:19:01 +0200

    1er commit

---
+++ main_page.py
@@ -0,0 +1,50 @@
+"""
+Menu : 
+    - manage chat_list/params/credentials/prompt
+    - chat
+    - open/search logs
+
+Comparatif des offres : 
+- Google : 1er module a priori gratuit
+- OpenAI : min 5€
+- Mistral : 1 mois gratuit, puis abonnement
+
+TODO : 
+- logger
+- add context
+
+"""
+
+from openai import OpenAI
+import streamlit as st
+
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
